ddad96708fa2302ac791199897e0c23db1ad530d
Martin Wimpress <code@flexion.org>
2009-04-22 11:55:15 +0100

    Improved release script

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Package for release
 
+NAME="IMDB-to-MPEG"
 VER=1.0
 
-bzr export IMDB-to-MPEG-v${VER}.tar
-tar --delete -f IMDB-to-MPEG-v${VER}.tar IMDB-to-MPEG-v${VER}/all_films.sh
-tar --delete -f IMDB-to-MPEG-v${VER}.tar IMDB-to-MPEG-v${VER}/release.sh
-gzip IMDB-to-MPEG-v${VER}.tar
+bzr export ${NAME}-v${VER}.tar
+tar --delete -f ${NAME}-v${VER}.tar ${NAME}-v${VER}/all_films.sh
+tar --delete -f ${NAME}-v${VER}.tar ${NAME}-v${VER}/release.sh
+gzip ${NAME}-v${VER}.tar
