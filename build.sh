2482d76854f9bc357f63a2ee23e2cc27a94dc8e0
Martin Wimpress <code@flexion.org>
2009-09-20 19:06:30 +0100

    Updated release script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 function build {
     RELEASE_NAME="IMDB-to-MPEG"
-    RELEASE_VER=2.0
+    RELEASE_VER="2.0"
+    RELEASE_DESCRIPTION="Create a MPEG video summarising a movie using data from IMDB"
+    RELEASE_KEYWORDS="IMDB, MPEG, video, summary, MP4, MPG, Mediatomb, tag, categorisation, ffmpeg"
 
     rm ${RELEASE_NAME}-v${RELEASE_VER}.tar* 2>/dev/null
     bzr export ${RELEASE_NAME}-v${RELEASE_VER}.tar
